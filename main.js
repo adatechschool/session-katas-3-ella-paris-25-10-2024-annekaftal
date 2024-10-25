c4ee8673dd8374d71dc8f7e17fe69316c22a769d
annekaftal <anne.kaftal@gmail.com>
2024-10-25 10:53:36 +0200

    Add: deux premières fonctions de l'étape 2

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
-const data = require("./pokedex.json");
-console.log(data);
+const pokemonData = require("./pokedex.json");
+// console.log(pokemonData);
+
+function countPokemon(data) {
+  console.log(data.pokemon.length);
+}
+
+function findHeavyPokemon(data) {
+  let heavyPokemon = [];
+  for (let i = 0; i < data.pokemon.length; i++) {
+    let weight = data.pokemon[i].weight;
+    let formattedWeight = parseFloat(weight.split(" ")[0]);
+    if (formattedWeight > 10) {
+      heavyPokemon.push(data.pokemon[i].name);
+    }
+  }
+  console.log(heavyPokemon);
+}
+
+countPokemon(pokemonData);
+findHeavyPokemon(pokemonData);
